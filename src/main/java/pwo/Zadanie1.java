b2cd1c8f49a79e25a3a4c88fa8ef5a37c8874de3
Marcin Żarnowski <marcin.zarnowski@pollub.edu.pl>
2023-10-28 14:26:45 +0200

    Dodanie generatora 100 pierwszych wyrazów ciągu Lucasa

--- src/main/java/pwo/Zadanie1.java
+++ src/main/java/pwo/Zadanie1.java
@@ -2,15 +2,24 @@ public class Zadanie1 {
 
     public static final int FROM = 0, TO = 100;
     public static final String FILE_NAME = "fibseq100.txt";
-
+    public static final String FILE_NAME_L = "lucseq100.txt";
     public static void main(String[] args) {
 
         System.out.println("Fibonacci wersja 2");
 
-        boolean ok = SequenceTools.writeToFile(
-                new FibonacciGenerator(), FROM, TO, FILE_NAME);
+        boolean ok = SequenceTools.writeToFile(new FibonacciGenerator(), FROM, TO, FILE_NAME);
 
         if (ok)
+            System.out.println("Wynik zapisany do pliku: "
+                    + FILE_NAME);
+        else
+            System.out.println("Błąd");
+        
+        System.out.println("Lucas wersja 1");
+
+        boolean ok2 = SequenceToolsL.writeToFile(new LucasGenerator(), FROM, TO, FILE_NAME_L);
+
+        if (ok2)
             System.out.println("Wynik zapisany do pliku: "
                     + FILE_NAME);
         else
